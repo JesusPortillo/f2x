99ff7f312b5287673a7d07d6b073fa1b6d1181ee
Jesus <jesusdavidpv12@gmail.com>
2025-09-12 23:08:16 -0500

    initial structure

---
+++ src/main/java/com/app/f2x/F2xApplication.java
@@ -0,0 +1,13 @@
+package com.app.f2x;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class F2xApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(F2xApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/f2x/F2xApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.f2x;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class F2xApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
